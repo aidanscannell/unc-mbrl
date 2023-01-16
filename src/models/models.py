3dd23e6a5b9dbeb8c2786244d5120d410f4b4755
Aidan Scannell <scannell.aidan@gmail.com>
2023-01-16 17:27:21 +0200

    updated base model

--- src/models/models.py
+++ src/models/models.py
@@ -1,27 +1,63 @@
 #!/usr/bin/env python3
 from typing import Callable, NamedTuple, Optional
-import torch.optim as optim
-import torch
+
 import pytorch_lightning as pl
-from src.types import ReplayBuffer
+import torch
+import torch.nn as nn
+import torch.distributions as td
+import torch.optim as optim
+from src.types import (
+    Action,
+    Observation,
+    Prediction,
+    ReplayBuffer,
+    ReplayBuffer_to_dynamics_DataLoader,
+)
 
 
-class Prediction(NamedTuple):
-    latent: torch.distributions.Distribution  # p(f_{\theta}(x) \mid x, \mathcal{D})
-    noise: torch.distributions.Distribution  # p(y \mid f_{\theta}(x))
-    output: torch.distributions.Distribution  # p(y \mid x, \mathcal{D})
-
-
-class DynamicsModel(pl.LightningModule):
-    def forward(self) -> Prediction:
+class DynamicsModel(nn.Module):
+    def predict(self, observation: Observation, action: Action) -> Prediction:
         raise NotImplementedError
 
-    def train(self, replay_buffer: ReplayBuffer):
+    def train(self, replay_buffer: ReplayBuffer, delta_state: bool = True):
         raise NotImplementedError
 
-    def training_step(self, batch, batch_idx):
-        raise NotImplementedError
 
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+# class DynamicsModel(pl.LightningModule):
+#     def __init__(
+#         self,
+#         model,
+#         trainer,
+#         batch_size: int = 64,
+#         num_workers: int = 1,
+#         shuffle: bool = True,
+#     ):
+#         self.model = model
+#         self.trainer = trainer
+#         self.batch_size = batch_size
+#         self.num_workers = num_workers
+#         self.shuffle = shuffle
+
+#     def predict(self, observation: Observation, action: Action) -> Prediction:
+#         x = torch.concat([observation, action], -1)
+#         return self.forward(x)
+
+#     def forward(self) -> Prediction:
+#         raise NotImplementedError
+
+#     def train(self, replay_buffer: ReplayBuffer, delta_state: bool = True):
+#         train_loader = ReplayBuffer_to_dynamics_DataLoader(
+#             replay_buffer=replay_buffer,
+#             batch_size=self.batch_size,
+#             shuffle=self.shuffle,
+#             delta_state=delta_state,
+#             num_workers=self.num_workers,
+#         )
+#         self.trainer.fit(self.model, train_loader)
+
+#     def training_step(self, batch, batch_idx):
+#         raise NotImplementedError
+
+#     def configure_optimizers(self):
+#         optimizer = optim.Adam(self.parameters(), lr=1e-3)
+#         return optimizer
